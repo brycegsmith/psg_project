bc12fa9c0856ae974db7f28067bb3c7132c7ea5b
andrew-inman <andrew2di@gmail.com>
2022-06-20 02:19:01 -0400

    Implemented emg and ecg feature extraction classes. Started main.ipynb file.

--- ecg.py
+++ ecg.py
@@ -1,36 +1,103 @@
 # This class should provide an easy way of managing ECG data.
 # It will also be where the ECG feature extraction lives.
 
+import heartpy as hp
+import pandas as pd
+import numpy as np
+
 class ECG:
 
-    def __init__(self, ecgData):
-        # Where ecgData is a dataframe with 2 columns: [Timestamp, ECG column of PSG]
+    def __init__(self, ecgData, windowSize = 240):
+        # Where ecgData is a dataframe with 2 columns: [Epoch, ECG column of PSG]
+        # windowSize is window duration in seconds
+
         self.ecgData = ecgData
+        self.lastEpoch = ecgData['epoch'].max()
 
-    def getBeats(self): # Find R waves of ECG signal
+        self.windowSizeSec = windowSize
+
+        # Constants:
+        samplingRate = 500
+        windowStepSec = 30 # Length of epoch
+
+        
+        rawEcg = np.array(ecgData["ECG1-ECG2"])
+        windowOverlap = (windowSize - windowStepSec)/windowSize
+        print(windowOverlap)
+        workingData, measures = hp.process_segmentwise( # Find heart beats and calculate heart rate metrics
+            rawEcg, 
+            samplingRate, 
+            segment_width = self.windowSizeSec, 
+            segment_overlap = windowOverlap,
+            calc_freq = True,
+            segment_min_size = self.windowSizeSec
+        )
+
+        self.workingData = workingData
+        self.measures = measures # Dict containing HR metrics (such as BPM, RMSSD, LF, HF, etc.)
+        
+
+    def getRawDataByEpoch(self, epoch):
+        # Return raw heart rate data [time, ECG] for the specified epoch (for visualization)
+        return self.ecgData[self.ecgData["epoch"] == epoch]
+
+    def getRawDataByTime(self, startTime, range):
+        startTime = startTime * 500 # startTime * samplingRate to convert from desired second to desired sample
+        range = range * 500 # Multiply by sampling rate to get number of datapoints 
+        endTime = startTime + range
+        return self.ecgData.loc[startTime:endTime, :]
+
+    def getMetrics(self, metrics = ["bpm", "rmssd", "lf", "hf", "lf/hf"]):
+        # Metrics is a list of metrics to access
+        # Returns a dataframe with format [epoch, metric1, metric2, etc]
+
+        # Metric options:
+        # bpm, ibi, sdnn, sdsd, rmssd, pnn50, pnn20, mad, lf, hf, lf/hf
+
+        startEpoch = int(self.ecgData['epoch'].min())
+        initializeEpochColumn = True
+        metricsDf = pd.DataFrame()
+
+        for metric in metrics:
+            if initializeEpochColumn:
+                thisMetricDf = pd.DataFrame(self.measures[metric], columns = [metric])
+                epochCount = thisMetricDf[metric].size
+                epochs = range(startEpoch, startEpoch + epochCount, 1)
+                metricsDf = pd.DataFrame(epochs, columns = ["epoch"])
+                metricsDf = metricsDf.join(thisMetricDf)
+                initializeEpochColumn = False
+            else:
+                thisMetricDf = pd.DataFrame(self.measures[metric], columns = [metric])
+                metricsDf = metricsDf.join(thisMetricDf)
+
+        return metricsDf
+
+
+
+    #def getBeats(self): # Find R waves of ECG signal
         # Return a dataframe: [timeOfBeat, amplitudeOfBeat/averageAmplitudeOfBeat]
-        pass
+        # pass
 
-    def heartRate(self, startTime=0, duration=0):
-        if startTime == 0 and duration == 0: 
+    #def heartRate(self, startTime=0, duration=0):
+        #if startTime == 0 and duration == 0: 
             # Go through entire dataset and calculate Average HR for every epoch
         # Call getBeats
         # Return a dataframe: [epoch number, instantaneoustimestamp heart rate]
 
 
-    def rri(self, startTime = 0, duration = 0):
+    #def rri(self, startTime = 0, duration = 0):
         # R-R intervals
         # Should probably normalize to average over entire dataset
     
-    def lf(self, startTime = 0, duration = 0):
-        if startTime == 0 and duration == 0: 
+    #def lf(self, startTime = 0, duration = 0):
+        #if startTime == 0 and duration == 0: 
             # Go through entire dataset and calculate Average LF power for every epoch
         # Call getBeats
         # Return a dataframe: [timestamp, lf power]
         # Should probably normalize to average over entire dataset
 
-    def hf(self, startTime = 0, duration = 0):
-        pass
+    #def hf(self, startTime = 0, duration = 0):
+        #pass
 
-    def rmssd(self, startTime = 0, duration = 0):
-        pass
+    #def rmssd(self, startTime = 0, duration = 0):
+        #pass
--- emg.py
+++ emg.py
@@ -1,8 +1,41 @@
 # This class should provide an easy way of managing EMG data.
 # It will also be where the EMG feature extraction lives.
 
+import numpy as np
+from scipy import signal
+import pandas as pd
+
+# Helper function to get energy of a signal:
+def energy(signal):
+    # Remove DC offset (find and subtract the mean)
+    dcOffset = signal.mean()
+    signal = signal - dcOffset
+
+    # Square the signal
+    signal = signal ** 2
+
+    # Sum the signal
+    return signal.sum()
+
 class EMG:
 
     def __init__(self, emgData):
         # Where emgData is a dataframe with 2 columns: [Timestamp, ECG column of PSG]
         self.emgData = emgData
+        self.rawEmg = np.array(emgData.loc[:, "EMG1-EMG2"])
+
+    def emgEnergy(self, filterOrder = 10, hpCutoff = 30, lpCutoff = 200):
+        # Filter
+        filterOrder = 10
+        hpCutoff = 30
+        lpCutoff = 200
+        samplingRate = 500
+        sosBandpass = signal.butter(filterOrder, (hpCutoff, lpCutoff), btype = "bandpass", fs = samplingRate, output = "sos")
+        filteredEmg = signal.sosfilt(sosBandpass, self.rawEmg)
+        self.emgData['filtered'] = filteredEmg
+
+        filteredWithEpochs = self.emgData[['epoch', 'filtered']]
+
+        filteredSignalGroupedByEpoch = (filteredWithEpochs.groupby(['epoch']))['filtered']
+        energyByEpoch = filteredSignalGroupedByEpoch.apply(energy)
+        return energyByEpoch
