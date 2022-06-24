3e688ef8ba15a0fe3a3b207e3c1919b87ba93208
Ashok Iyer <105729578+aiyer98@users.noreply.github.com>
2022-06-24 15:52:42 -0400

    Added ECB using Welch Power Spectrum

--- eog.py
+++ eog.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from scipy import signal as sig
 
 class EOG:
 
@@ -21,8 +23,18 @@ class EOG:
         # Area Under Curve
         EOG_AUC = (self.absrawEOG).groupby('epoch').sum()
 
+        # Energy Content Band using Power Spectrum
+        ECB = []
+        for i in range(0, self.rawEOG['epoch'].nunique()):
+            focus = self.rawEOG[self.rawEOG['epoch'] == i]
+            f, ps = np.array(sig.welch(focus['ROC-LOC'], nperseg = 2048, scaling='spectrum', fs=512))
+            bandenergy = ps[(f >= 0.35) & (f <= 0.5)].sum()
+            ECB.append(bandenergy)
+
+        # Data Assembly
         final_EOG = pd.concat([pd.Series([i for i in range(0, len(EOG_PAV))]), EOG_PAV, EOG_VAV, EOG_STD, EOG_AUC], axis=1)
         final_EOG.columns = ['epoch', 'EOG_PAV', 'EOG_VAV', 'EOG_STD', 'EOG_AUC']
+        final_EOG['EOG_ECB'] = ECB
         return final_EOG
 
 
