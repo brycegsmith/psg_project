6d719c4bf922f63d0ba9520ccc57b9ffa747b0d0
andrew-inman <andrew2di@gmail.com>
2022-06-24 14:42:42 -0400

    Added training datasets through nfle15

--- constants.py
+++ constants.py
@@ -15,7 +15,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 15,
-        "study_start_second": 42,
+        "study_start_second": 42
     },
     "n10": {
         "study_start_year": 2008,
@@ -23,8 +23,112 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 24,
-        "study_start_second": 52,
+        "study_start_second": 52
     },
+    "ins2": {
+        "study_start_year": 2007,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 18,
+        "study_start_min": 25,
+        "study_start_second": 37
+    },
+    "ins5": {
+        "study_start_year": 2011,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 17,
+        "study_start_min": 58,
+        "study_start_second": 48
+    },
+    "ins6": {
+        "study_start_year": 2002,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 18,
+        "study_start_min": 46,
+        "study_start_second": 57
+    },
+    "ins7": {
+        "study_start_year": 2011,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 19,
+        "study_start_min": 58,
+        "study_start_second": 14
+    },
+    "n11": {
+        "study_start_year": 2007,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 22,
+        "study_start_min": 36,
+        "study_start_second": 46
+    },
+    "n1": {
+        "study_start_year": 2009,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 22,
+        "study_start_min": 6,
+        "study_start_second": 3
+    },
+    "n2": {
+        "study_start_year": 2009,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 19,
+        "study_start_min": 12,
+        "study_start_second": 6
+    },
+    "nfle10": {
+        "study_start_year": 2010,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 23,
+        "study_start_min": 50,
+        "study_start_second": 31
+    },
+    "nfle11": {
+        "study_start_year": 2011,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 22,
+        "study_start_min": 30,
+        "study_start_second": 42
+    },
+    "nfle12": {
+        "study_start_year": 2007,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 22,
+        "study_start_min": 40,
+        "study_start_second": 41
+    },
+    "nfle13": {
+        "study_start_year": 2001,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 22,
+        "study_start_min": 7,
+        "study_start_second": 5
+    },
+    "nfle14": {
+        "study_start_year": 2001,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 21,
+        "study_start_min": 46,
+        "study_start_second": 43
+    },
+    "nfle15": {
+        "study_start_year": 1999,
+        "study_start_month": 1,
+        "study_start_day": 1,
+        "study_start_hour": 22,
+        "study_start_min": 7,
+        "study_start_second": 22
+    }
 }
 
 # FREQUENCY
