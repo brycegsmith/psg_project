fb18056ec48dd7d347243eeba994e82c2af397c1
Bryce Smith <brycegsmith@hotmail.com>
2022-06-28 01:21:09 -0400

    Updated EEG columns

--- constants.py
+++ constants.py
@@ -15,7 +15,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 15,
-        "study_start_second": 42
+        "study_start_second": 42,
     },
     "n10": {
         "study_start_year": 2008,
@@ -23,7 +23,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 24,
-        "study_start_second": 52
+        "study_start_second": 52,
     },
     "ins2": {
         "study_start_year": 2007,
@@ -31,7 +31,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 18,
         "study_start_min": 25,
-        "study_start_second": 37
+        "study_start_second": 37,
     },
     "ins5": {
         "study_start_year": 2011,
@@ -39,7 +39,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 17,
         "study_start_min": 58,
-        "study_start_second": 48
+        "study_start_second": 48,
     },
     "ins6": {
         "study_start_year": 2002,
@@ -47,7 +47,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 18,
         "study_start_min": 46,
-        "study_start_second": 57
+        "study_start_second": 57,
     },
     "ins7": {
         "study_start_year": 2011,
@@ -55,7 +55,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 19,
         "study_start_min": 58,
-        "study_start_second": 14
+        "study_start_second": 14,
     },
     "n11": {
         "study_start_year": 2007,
@@ -63,7 +63,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 36,
-        "study_start_second": 46
+        "study_start_second": 46,
     },
     "n1": {
         "study_start_year": 2009,
@@ -71,7 +71,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 6,
-        "study_start_second": 3
+        "study_start_second": 3,
     },
     "n2": {
         "study_start_year": 2009,
@@ -79,7 +79,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 19,
         "study_start_min": 12,
-        "study_start_second": 6
+        "study_start_second": 6,
     },
     "nfle10": {
         "study_start_year": 2010,
@@ -87,7 +87,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 23,
         "study_start_min": 50,
-        "study_start_second": 31
+        "study_start_second": 31,
     },
     "nfle11": {
         "study_start_year": 2011,
@@ -95,7 +95,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 30,
-        "study_start_second": 42
+        "study_start_second": 42,
     },
     "nfle12": {
         "study_start_year": 2007,
@@ -103,7 +103,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 40,
-        "study_start_second": 41
+        "study_start_second": 41,
     },
     "nfle13": {
         "study_start_year": 2001,
@@ -111,7 +111,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 7,
-        "study_start_second": 5
+        "study_start_second": 5,
     },
     "nfle14": {
         "study_start_year": 2001,
@@ -119,7 +119,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 21,
         "study_start_min": 46,
-        "study_start_second": 43
+        "study_start_second": 43,
     },
     "nfle15": {
         "study_start_year": 1999,
@@ -127,7 +127,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 7,
-        "study_start_second": 22
+        "study_start_second": 22,
     },
     "plm10": {
         "study_start_year": 2011,
@@ -135,7 +135,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 41,
-        "study_start_second": 46
+        "study_start_second": 46,
     },
     "plm1": {
         "study_start_year": 2010,
@@ -143,7 +143,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 21,
         "study_start_min": 58,
-        "study_start_second": 23
+        "study_start_second": 23,
     },
     "plm2": {
         "study_start_year": 2007,
@@ -151,7 +151,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 28,
-        "study_start_second": 47
+        "study_start_second": 47,
     },
     "plm3": {
         "study_start_year": 2011,
@@ -159,7 +159,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 57,
-        "study_start_second": 41
+        "study_start_second": 41,
     },
     "plm5": {
         "study_start_year": 2009,
@@ -167,7 +167,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 20,
         "study_start_min": 16,
-        "study_start_second": 16
+        "study_start_second": 16,
     },
     "plm6": {
         "study_start_year": 2007,
@@ -175,7 +175,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 18,
         "study_start_min": 28,
-        "study_start_second": 17
+        "study_start_second": 17,
     },
     "rbd10": {
         "study_start_year": 2008,
@@ -183,7 +183,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 9,
-        "study_start_second": 22
+        "study_start_second": 22,
     },
     "rbd11": {
         "study_start_year": 2008,
@@ -191,7 +191,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 9,
-        "study_start_second": 22
+        "study_start_second": 22,
     },
     "rbd12": {
         "study_start_year": 2008,
@@ -199,7 +199,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 21,
         "study_start_min": 52,
-        "study_start_second": 54
+        "study_start_second": 54,
     },
     "rbd13": {
         "study_start_year": 2007,
@@ -207,7 +207,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 19,
-        "study_start_second": 6
+        "study_start_second": 6,
     },
     "rbd16": {
         "study_start_year": 2008,
@@ -215,7 +215,7 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 22,
         "study_start_min": 15,
-        "study_start_second": 16
+        "study_start_second": 16,
     },
     "rbd17": {
         "study_start_year": 2009,
@@ -223,9 +223,43 @@ startTimes = {
         "study_start_day": 1,
         "study_start_hour": 21,
         "study_start_min": 54,
-        "study_start_second": 31
-    }
+        "study_start_second": 31,
+    },
 }
 
 # FREQUENCY
 FREQUENCY = 512
+
+# EEG_COLUMNS (Used for EEG channel averaging)
+EEG_COLUMNS = [
+    "Fp2-F4",
+    "F4-C4",
+    "C4-P4",
+    "P4-O2",
+    "F8-T4",
+    "T4-T6",
+    "FP1-F3",
+    "F3-C3",
+    "C3-P3",
+    "P3-O1",
+    "F7-T3",
+    "T3-T5",
+    "C4-A1",
+    "ROC-LOC",
+    "LOC-ROC",
+    "LOC / A2",
+    "ROC / A1",
+    "C3-A2",
+    "O1-A2",
+    "O2-A1",
+    "ROC",
+    "LOC",
+    "F3A2",
+    "F4A1",
+    "C3A2",
+    "C4A1",
+    "F2-F4",
+    "F1-F3",
+    "LOC-A1",
+    "ROC-A2",
+]
